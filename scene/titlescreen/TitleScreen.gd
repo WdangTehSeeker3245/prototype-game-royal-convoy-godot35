ffab700acaf08686066c093ac4a879a66faf5f84
WdangTehSeeker3245 <faizalnf3245@gmail.com>
2023-09-28 05:24:15 +0700

    first commit, title screen and character selection layout only

---
+++ scene/titlescreen/TitleScreen.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _ready():
+	var bgm = $BGM_Main
+	bgm.play()
+	
+func _on_NewGameButton_pressed():
+	var decision = $Decision_SE
+	decision.play()
+	get_tree().change_scene("res://scene/character_story_selection/CharacterStorySelection.tscn")
+
